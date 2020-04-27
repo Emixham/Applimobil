fa86447e3f480fec29cd6792dfe9de4f5c6b8ee9
Emixham <Maximed.ipod@gmail.com>
2020-04-27 15:18:35 +0200

    ajout du splashscreen

---
+++ app/src/main/java/fr/epsi/applimobil/SplashScreen.java
@@ -0,0 +1,26 @@
+package fr.epsi.applimobil;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+
+public class SplashScreen extends Activity {
+
+    private static int SPLASH_TIME_OUT = 2000;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run(){
+                Intent i = new Intent(SplashScreen.this,MainActivity.class);
+                startActivity(i);
+            }
+        },SPLASH_TIME_OUT);
+    }
+}
