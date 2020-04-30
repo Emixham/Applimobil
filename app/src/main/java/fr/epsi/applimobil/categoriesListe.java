9bb83431d9449eb8c4dbd30a0f4a74947fb10628
canneberge <kevfyrezz@gmail.com>
2020-04-30 14:10:08 +0200

    wip4

--- app/src/main/java/fr/epsi/applimobil/categoriesListe.java
+++ app/src/main/java/fr/epsi/applimobil/categoriesListe.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.view.View;
 import android.widget.Adapter;
 import android.widget.AdapterView;
@@ -19,20 +20,21 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class categoriesListe extends AppCompatActivity {
+public class categoriesListe extends AppliActivity {
 
     private CategorieAdapter categorieAdapter;
     private ArrayList categoriesListe;
 
-    public static void display(AppCompatActivity activity) {
+    public static void display(AppliActivity activity , Categorie categorie) {
        Intent intent = new Intent(activity, categoriesListe.class);
+       intent.putExtra("categorie", (Parcelable) categorie) ;
        activity.startActivity(intent);
    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categories_liste);
-        
+        showBackBtn();
         categoriesListe = new ArrayList<>();
         ListView listView = findViewById(R.id.listView_categories);
         categorieAdapter = new CategorieAdapter(this, R.layout.c_categories, categoriesListe);
@@ -42,13 +44,13 @@ public class categoriesListe extends AppCompatActivity {
 
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-               //ProductListActivity.display(CategorieActivity.this,categories.get(position));
+               //ProductListActivity.display(CategorieActivity.this,categoriesListe.get(position));
             }
         });
 
         String url = "http://djemam.com/epsi/categories.json";
 
-        new HttpRequest(url, new HttpRequest.HttpAsyncTaskListener() {
+        new  HttpRequest(url, new HttpRequest.HttpAsyncTaskListener() {
             @Override
             public void webServiceDone(String result) {
                 initData(result);
@@ -67,8 +69,8 @@ public class categoriesListe extends AppCompatActivity {
             jsonObject=new JSONObject(data);
             JSONArray jsonArray= jsonObject.getJSONArray("items");
             for(int i=0;i<jsonArray.length();i++){
-                Categorie categorie =new Categorie(jsonArray.getJSONObject(i));
-                ArrayAdapter adapter = new ArrayAdapter<String>(this,R.layout.activity_categories_liste,R.id.listView_categories , Collections.singletonList(categorie.title));
+                Categorie categorie=new Categorie(jsonArray.getJSONObject(i));
+                categoriesListe.add(categorie);
             }
 
         } catch (JSONException e) {
